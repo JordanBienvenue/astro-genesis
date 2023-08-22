5ddb9843f2dc669803187dfa3f50f0221777b2c6
Jordan Bienvenue <jord.bienvenue@gmail.com>
2023-08-22 08:46:12 +0400

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,57 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				dark: {
+					...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+					"primary": "blue",
+					"primary-focus": "mediumblue",
+				},
+			},
+			{
+				one: {
+					"primary": "#a991f7",
+					"secondary": "#f6d860",
+					"accent": "#37cdbe",
+					"neutral": "#3d4451",
+					"base-100": "#ffffff",
+				},
+			},
+			// "light",
+			// "dark",
+			// "cupcake",
+			// "bumblebee",
+			// "emerald",
+			// "corporate",
+			// "synthwave",
+			// "retro",
+			// "cyberpunk",
+			// "valentine",
+			// "halloween",
+			// "garden",
+			// "forest",
+			// "aqua",
+			// "lofi",
+			// "pastel",
+			// "fantasy",
+			// "wireframe",
+			// "black",
+			// "luxury",
+			// "dracula",
+			// "cmyk",
+			// "autumn",
+			// "business",
+			// "acid",
+			// "lemonade",
+			// "night",
+			// "coffee",
+			"winter",
+		],
+	},
+}
